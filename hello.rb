4745b11cf40d08cdade08df4df288e37f0b52fe4
armi1189 <armi1189@gmail.com>
2015-03-30 11:43:44 +0100

    Add some html

--- hello.rb
+++ hello.rb
@@ -1,9 +1,11 @@
 require 'sinatra'
 
 get '/' do
-  'hello!'
+  "<div>
+    <img src='http://bit.ly/1eze8aE'>
+   </div>"
 end
 
 get '/secret' do
-  'this is a secret page'
+  'this is a secret page. and this is a reload test'
 end
